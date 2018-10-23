22762357e3de2c1e6239d05308798381de2a89ff
ICEPOR\jsivaganesh <jsivaganesh@ivycomptech.com>
2018-10-23 21:58:57 +0530

    cucumber

---
+++ src/test/java/com/sample/spark/spark_java_eaxmple/RunCukesTest.java
@@ -0,0 +1,12 @@
+package com.sample.spark.spark_java_eaxmple;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty"})
+public class RunCukesTest {
+
+}
